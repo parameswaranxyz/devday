d42f6b6e974bcac81df31236d7f6b03d60307d97
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-25 20:23:29 +0530

    Add DevdayEvent interface definition

--- _js/definitions.ts
+++ _js/definitions.ts
@@ -4,11 +4,62 @@ import { Stream } from 'xstream';
 import { VNode } from '@cycle/dom';
 
 export interface Sources {
-    dom: DOMSource;
-    routes: RoutesSource;
+  dom: DOMSource;
+  routes: RoutesSource;
 }
 
 export interface Sinks {
-    dom: Stream<VNode>;
-    routes: Stream<string>;
+  dom: Stream<VNode>;
+  routes: Stream<string>;
+}
+
+export interface EventTime {
+  start_time: Date;
+  end_time?: Date;
+}
+
+export interface Address {
+  line_one: string;
+  line_two?: string;
+  landmark?: string;
+  locality?: string;
+  city: string;
+  zip: number;
+}
+
+export enum AgendaEntryType {
+  Talk,
+  Break,
+  Workshop
+}
+
+export interface Author {
+  name: string;
+  twitter_handle: string;
+  github_username: string;
+  facebook_username: string;
+  linkedin_profile_url: string;
+  image_url: string;
+}
+
+export interface AgendaEntry {
+  type: AgendaEntryType;
+  title: string;
+  authors: Author[];
+  time: EventTime;
+  abstract: string;
+}
+
+export interface DevdayEvent {
+  title: string;
+  url: string;
+  categories: string[];
+  tags: string[];
+  author: string;
+  abstract: string;
+  event_time: EventTime;
+  publish_time: Date;
+  registration_time: EventTime;
+  venue: Address;
+  agenda: AgendaEntry[];
 }
