d2b34a7b948a7a0e2572a735c282d33c969a6079
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-25 21:11:02 +0530

    Link archive to main

--- _js/archive.ts
+++ _js/archive.ts
@@ -1,45 +1,84 @@
-import xs from 'xstream';
+import { Stream } from 'xstream';
 import { run } from '@cycle/xstream-run';
-import { div, article, a, i, span, header, h4, h5, footer, makeDOMDriver } from '@cycle/dom';
+import { div, article, a, img, i, span, header, nav, main, h4, h5, footer, makeDOMDriver, VNode } from '@cycle/dom';
+import { Sources, Sinks, BaseUrlProvider } from './definitions';
 
-function getDisplayTime(time) {
-  var timeSplits = new Date(time).toString().split(' ');
+function getDisplayTime(date: Date) {
+  var timeSplits = date.toString().split(' ');
   return timeSplits[2] + ' ' + timeSplits[1] + ' ' + timeSplits[3];
 }
 
-function main({ dom }) {
-  return {
-    dom: xs.periodic(200)
-      .map(n =>
-        div('.panel', window.events
-          .filter(event => new Date(event.time) - new Date() < 0)
-          .sort((a, b) => new Date(b.time) - new Date(a.time))
-          .map(event =>
-            article('.centered', [
-              div('.event.card', [
-                a('.go.to.event.button', { props: { href: window.baseUrl + event.url } }, [
-                  i('.material-icons', 'arrow_forward'),
-                  span('.hidden', 'Go')
-                ]),
-                header([
-                  h4([event.name])
-                ]),
-                div('.content', [
-                  h5(getDisplayTime(event.time)),
-                  event.abstract
-                ]),
-                footer([
-                  i('.material-icons', 'label'),
-                  ...event.tags.map(tag =>
-                    a('.tag', { props: { href: window.baseUrl + '/tags/' + tag.replace(' ', '-') } }, tag)
-                  ),
-                  a('.right.button', { props: { href: window.baseUrl + event.url } }, 'View Event')
+function renderHeader(baseUrl: string): VNode {
+  return header([
+    div('.container', [
+      div('.content', [
+        a('.title', { props: { href: baseUrl + '/' } }, [
+          img({ props: { src: baseUrl + 'images/logo.gif' } })
+        ]),
+        div('.navigation.container', [
+          nav([
+            a({ props: { href: baseUrl + '/archive/' } }, 'Archive')
+          ])
+        ])
+      ])
+    ])
+  ]);
+}
+
+function archive(sources: Sources): Sinks {
+  const route$ = sources.routes.route$;
+  const events$ = sources.events.events$;
+  const baseUrl = (window as BaseUrlProvider).baseUrl;
+  const currentDate = new Date();
+  const vdom$ =
+    route$
+      .filter(url => url === 'archive')
+      .map(route =>
+        events$.map(events =>
+          div('.devday.archive', [
+            div('.container', [
+              div('.layout', [
+                renderHeader(baseUrl),
+                main([
+                  div('.panel', events
+                    .filter(event => event.event_time.start_time - currentDate < 0)
+                    .sort((a, b) => b.event_time.start_time - a.event_time.start_time)
+                    .map(event =>
+                      article('.centered', [
+                        div('.event.card', [
+                          a('.go.to.event.button', { props: { href: baseUrl + event.url } }, [
+                            i('.material-icons', 'arrow_forward'),
+                            span('.hidden', 'Go')
+                          ]),
+                          header([
+                            h4([event.title])
+                          ]),
+                          div('.content', [
+                            h5(getDisplayTime(event.event_time.start_time)),
+                            event.abstract
+                          ]),
+                          footer([
+                            i('.material-icons', 'label'),
+                            ...event.tags.map(tag =>
+                              a('.tag', { props: { href: baseUrl + '/tags/' + tag.replace(' ', '-') } }, tag)
+                            ),
+                            a('.right.button', { props: { href: baseUrl + event.url } }, 'View Event')
+                          ])
+                        ])
+                      ])
+                    ))
                 ])
               ])
             ])
-          ))
-      )
+          ])
+        )
+      ).flatten();
+
+  return {
+    dom: vdom$,
+    routes: null,
+    events: null
   };
 }
 
-run(main, { dom: makeDOMDriver('main') });
+export default archive;
--- _js/main.ts
+++ _js/main.ts
@@ -1,10 +1,17 @@
 import { Sources, Sinks } from './definitions';
+import archive from './archive';
+import xs from 'xstream';
 
 function main(sources: Sources): Sinks {
+  const archiveSinks = archive(sources);
+
+  const vdom$ = xs.merge(archiveSinks.dom);
+  const route$ = xs.merge(archiveSinks.routes);
+  const event$ = xs.merge(archiveSinks.events);
   return {
-    dom: null,
-    routes: null,
-    events: null
+    dom: vdom$,
+    routes: route$,
+    events: event$ 
   };
 }
 
