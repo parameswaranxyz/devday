718c1d57547efbb0bf9ee3d8943ad5f35d420ae3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-28 12:49:26 +0530

    Add sass files to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
-var webpack = require('webpack');  
-module.exports = {  
-  entry: './src/app.ts',
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './src/app.ts',
+    './css/home.scss',
+    './css/archive.scss',
+    './css/event.scss'
+  ],
   output: {
-    filename: './dist/app.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/'
   },
+  debug: true,
   devtool: 'source-map',
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
   },
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin()
-  // ],
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    // new webpack.optimize.UglifyJsPlugin()
+  ],
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts' }
+      { test: /\.ts$/, loader: 'ts' },
+      { test: /\.scss$/, loader: "style!css!autoprefixer!sass" }
     ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./_sass")]
   }
-}
+};
