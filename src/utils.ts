e750033ab229a2e873182c592f9c67a8cfef5898
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-28 20:55:56 +0530

    Add safety to pluck

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { Stream } from 'xstream';
 
 export function pluck<T,S>(stream: Stream<T>, propertyName: string): Stream<S> {
-  return stream.map(str => str[propertyName]).flatten();
+  return stream.map(str => str[propertyName] || Stream.empty()).flatten();
 }
