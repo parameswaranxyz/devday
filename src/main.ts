cef0a4d05d1951b3bd0eb2389e214430bd64f3d0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-02-28 18:12:08 +0530

    Update main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { Sources, Sinks } from './definitions';
-import home from './home';
+import { routes } from './routes';
+import { Location } from 'history';
 import xs from 'xstream';
+import switchPath from 'switch-path';
+import { pluck } from './utils';
+import { VNode } from '@cycle/dom';
+import { RegistrationRequest } from './drivers/registrations';
 
 function main(sources: Sources): Sinks {
-  return home(sources);
+  const history$: xs<Location> = sources.history;
+  const component$ = history$.map(route => routes[route.pathname] as (sources: Sources) => Sinks);
+  const sinks$ = component$.map(component => component(sources));
+  return {
+    dom: pluck<Sinks, VNode>(sinks$, 'dom'),
+    routes: pluck<Sinks, string>(sinks$, 'routes'),
+    events: pluck<Sinks, string>(sinks$, 'events'),
+    prevent: pluck<Sinks, Event>(sinks$, 'prevent'),
+    registrations: pluck<Sinks, RegistrationRequest>(sinks$, 'registrations'),
+    history: pluck<Sinks, string>(sinks$, 'history')
+  };
 }
 
 export default main;
