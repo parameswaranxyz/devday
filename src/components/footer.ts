45c10a707ca353838572302cd0af88f1ef798636
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-03-01 10:59:03 +0530

    Refactor out footer as a component

---
+++ src/components/footer.ts
@@ -0,0 +1,43 @@
+import xs from 'xstream';
+import { VNode, footer, div, a, span, p } from '@cycle/dom';
+
+interface FooterSinks {
+  dom: xs<VNode>;
+}
+
+export function Footer(): FooterSinks {
+  const vtree$ = xs.of(footer([
+    div('.left.section', [
+      a('.twitter.social.button', {
+        props: {
+          href: 'https://twitter.com/devday_',
+          target: '_blank'
+        }
+      }, [
+          span('.hidden', 'twitter')
+        ]),
+      a('.facebook.social.button', {
+        props: {
+          href: 'https://facebook.com/d3vday',
+          target: '_blank'
+        }
+      }, [
+          span('.hidden', 'facebook')
+        ])
+    ]),
+    div('.right.section', [
+      p([
+        '© 2016 - Organised by ',
+        a('.sahaj.org.link', {
+          props: {
+            href: 'https://sahajsoft.com',
+            target: '_blank'
+          }
+        }, 'Sahaj Software Solutions')
+      ])
+    ])
+  ]));
+  return {
+    dom: vtree$
+  };
+}
