5ca821a021668b93c6a6a41defce0783f840cd34
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-03-02 15:24:28 +0530

    Refactor out common utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,15 @@
 import { Stream } from 'xstream';
 
-export function pluck<T,U>(stream: Stream<T>, getter: (single: T) => Stream<U>): Stream<U> {
+export function pluck<T, U>(stream: Stream<T>, getter: (single: T) => Stream<U>): Stream<U> {
   return stream.map(str => getter(str) || Stream.empty()).flatten();
+}
+
+export const fadeInOutStyle = {
+  opacity: '0', delayed: { opacity: '1' }
+};
+
+export function pad(n: string, width: number, z?: string): string {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
