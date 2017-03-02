8714c555500c7d1a7f3c289deb1c57eff83dfd18
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-03-02 15:25:35 +0530

    Use updated routing mechanism

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { Sources, Sinks } from './definitions';
-import { routes } from './routes';
-import { Location } from 'history';
-import xs from 'xstream';
-import switchPath from 'switch-path';
-import { pluck } from './utils';
-import { VNode } from '@cycle/dom';
-import { RegistrationRequest } from './drivers/registrations';
+import { resolve } from './routes';
+import { Stream } from 'xstream';
 import { Layout } from './components/layout';
 
 function main(sources: Sources): Sinks {
-  const history$: xs<Location> = sources.history;
-  const component$ = history$.map(route => routes[route.pathname] as (sources: Sources) => Sinks);
-  return Layout({...sources, component$});
+  const history$: Stream<Location> = sources.history.debug();
+  const sinks$ =
+    history$
+      .map(route => resolve(route.hash))
+      .map(resolution => resolution.component({...sources, ...resolution.sources}));
+  return Layout({...sources, sinks$});
 }
 
 export default main;
