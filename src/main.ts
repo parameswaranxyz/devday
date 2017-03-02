807dd972e7f52f04b600ee21c19bd09077f3ff21
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-03-02 16:03:07 +0530

    Remove unnecessary debugging info

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { Stream } from 'xstream';
 import { Layout } from './components/layout';
 
 function main(sources: Sources): Sinks {
-  const history$: Stream<Location> = sources.history.debug();
   const sinks$ =
-    history$
+    (sources.history as Stream<Location>)
       .map(route => resolve(route.hash))
       .map(resolution => resolution.component({...sources, ...resolution.sources}));
   return Layout({...sources, sinks$});
