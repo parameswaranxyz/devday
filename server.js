5ff9d4592f5eb6e8628472a5e851d4205460efb6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-18 16:42:35 +0530

    Enable CORS

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ let registrationStore = require('./backend/registration_store');
 let attendeeCount = require('./backend/attendee_count');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('./')); // for static file serving
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(express.static('./'));
 
 app.get('/attendees/:event_url', (req, res) => {
   let query = req.query;
