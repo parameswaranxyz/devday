a210ed2abb00f8fac98c252dfbea18594dbe4bb9
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-18 16:43:26 +0530

    Add webpack v3 configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,189 @@
+var argv = require('yargs').argv;
 var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var debug = require('debug')('app:config:webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.ts',
-  output: {
-    filename: './dist/app.js'
-  },
-  debug: true,
+// Environment Constants
+var NODE_ENV = process.env.NODE_ENV;
+var API_ENDPOINT = JSON.stringify(process.env.API_ENDPOINT);
+var APP_URL = JSON.stringify(process.env.APP_URL);
+var __DEV__ = NODE_ENV === 'development';
+var __PROD__ = NODE_ENV === 'production';
+var __TEST__ = NODE_ENV === 'test';
+var __COVERAGE__ = !argv.watch && __TEST__;
+var __BASENAME__ = JSON.stringify(process.env.BASENAME || '');
+var GLOBALS = {
+  'process.env': { NODE_ENV: JSON.stringify(NODE_ENV) },
+  NODE_ENV: NODE_ENV,
+  __DEV__: __DEV__,
+  __PROD__: __PROD__,
+  __TEST__: __TEST__,
+  __COVERAGE__: __COVERAGE__,
+  __BASENAME__: __BASENAME__,
+  API_ENDPOINT: API_ENDPOINT,
+  APP_URL: APP_URL
+};
+
+// Constants
+var ROOT = path.resolve(__dirname);
+var DIST = path.join(ROOT, 'dist');
+var SRC = path.join(ROOT, 'src');
+var PROJECT_PUBLIC_PATH = '/';
+
+// Base Configuration
+var webpackConfig = {
+  name: 'client',
+  target: 'web',
   devtool: 'source-map',
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    extensions: ['.ts', '.tsx', '.js', '.json']
   },
-  plugins: [
-    // new webpack.NoErrorsPlugin(),
-    // new webpack.optimize.UglifyJsPlugin()
-  ],
-  module: {
-    loaders: [
-      { test: /\.ts$/, loader: 'ts' }
-    ]
-  }
+  module: { rules: [] }
 };
+
+// Entry
+var APP_ENTRY = path.join(ROOT, 'src/app.ts');
+var WEBPACK_DEV_SERVER = `webpack-dev-server/client?path=${PROJECT_PUBLIC_PATH}`
+webpackConfig.entry = {
+  app: __DEV__
+    ? [WEBPACK_DEV_SERVER, APP_ENTRY]
+    : [APP_ENTRY],
+  vendor: [
+    '@cycle/run',
+    '@cycle/history',
+    '@cycle/http',
+    '@cycle/isolate',
+    '@cycle/dom',
+    'xstream',
+    'switch-path'
+  ]
+};
+
+// Output
+webpackConfig.output = {
+  filename: `[name].[hash].js`,
+  path: DIST,
+  publicPath: PROJECT_PUBLIC_PATH
+};
+
+// Plugins
+webpackConfig.plugins = [
+  new webpack.DefinePlugin(GLOBALS),
+  new HtmlWebpackPlugin({
+    template: path.join(SRC, 'index.html'),
+    hash: false,
+    favicon: path.join(SRC, 'favicon.ico'),
+    filename: 'index.html',
+    inject: 'body',
+    minify: { collapseWhitespace: true }
+  }),
+  new CopyWebpackPlugin([
+    { from: 'src/images', to: 'images' },
+    { from: 'src/fonts', to: 'fonts' }
+  ])
+];
+
+if (__DEV__) {
+  debug('Enabling plugins for live development (HMR, NoErrors).')
+  webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+} else if (__PROD__) {
+  debug('Enabling plugins for production (OccurrenceOrder & UglifyJS).')
+  webpackConfig.plugins.push(
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  )
+}
+
+// Don't split bundles during testing, since we only want import one bundle
+if (!__TEST__) {
+  webpackConfig.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor']
+    })
+  )
+}
+
+// Rules
+var CSS_LOADERS = [
+  'style-loader?sourceMap',
+  'css-loader?sourceMap&-minimize',
+  'postcss-loader?sourceMap'
+];
+function addRules(rules) {
+  webpackConfig.module.rules = webpackConfig.module.rules.concat(rules);
+}
+// TypeScript and source maps
+addRules([
+  { test: /\.tsx?$/, loader: 'ts-loader' },
+  { test: /\.js$/, loader: 'source-map-loader', enforce: 'pre' }
+]);
+// Styles
+addRules([
+  {
+    test: /\.scss$/,
+    loaders: [
+      ...CSS_LOADERS,
+      'sass-loader?sourceMap'
+    ]
+  },
+  {
+    test: /\.css$/,
+    loaders: CSS_LOADERS
+  }
+]);
+webpackConfig.plugins.push(
+  new webpack.LoaderOptionsPlugin({ sassLoader: { includePaths: path.join(SRC, 'styles') } })
+);
+// webpackConfig.plugins.push(
+//   new webpack.ProvidePlugin({
+//     $: "jquery",
+//     jQuery: "jquery"
+//   })
+// );
+// Files
+addRules([
+  { test: /\.woff(\?.*)?$/, loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff' },
+  { test: /\.woff2(\?.*)?$/, loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2' },
+  { test: /\.otf(\?.*)?$/, loader: 'file-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype' },
+  { test: /\.ttf(\?.*)?$/, loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream' },
+  { test: /\.eot(\?.*)?$/, loader: 'file-loader?prefix=fonts/&name=[path][name].[ext]' },
+  { test: /\.svg(\?.*)?$/, loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
+  { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+]);
+
+// when we don't know the public path (we know it only when HMR is enabled [in development]) we
+// need to use the extractTextPlugin to fix this issue:
+// http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
+if (!__DEV__) {
+  debug('Applying ExtractTextPlugin to CSS loaders.')
+  webpackConfig.module.rules.filter(rule =>
+    rule.loaders && rule.loaders.find((name) => /css/.test(name.split('?')[0]))
+  ).forEach(loader => {
+    const first = loader.loaders[0];
+    const rest = loader.loaders.slice(1);
+    loader.loader = ExtractTextPlugin.extract({ fallback: first, use: rest.join('!') });
+    delete loader.loaders;
+  })
+
+  webpackConfig.plugins.push(
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash].css',
+      allChunks: true
+    })
+  )
+}
+
+module.exports = webpackConfig;
