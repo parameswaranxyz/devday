6c20cf3397954714a3b3dd1d2c63fc16ff309ed3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-19 15:46:41 +0530

    Use text field component for rendering

--- src/routes/Home/components/TalkRegistration/index.ts
+++ src/routes/Home/components/TalkRegistration/index.ts
@@ -1,5 +1,6 @@
 import { Stream } from 'xstream';
 import { VNode, section, div, h4, a, form } from '@cycle/dom';
+import { TextField } from '../../../../components/TextField';
 import './styles.scss';
 
 interface Sinks {
@@ -7,21 +8,39 @@ interface Sinks {
 }
 
 export const TalkRegistration = (): Sinks => {
-  const vdom$ = Stream.of(
-    section('.talk-registration', [
-      div('.title', [
-        h4(['Present your own idea'])
-      ]),
-      div('.content', [
-        form([
-          
+  const nameDom$ = TextField({
+    id$: Stream.of('talk-name'),
+    error$: Stream.of('Please enter a valid name!'),
+    label$: Stream.of('Name'),
+    pattern$: Stream.of('^[a-zA-Z][a-zA-Z ]{4,}'),
+    required$: Stream.of(true)
+  }).dom;
+  const mobileDom$ = TextField({
+    id$: Stream.of('talk-mobile'),
+    error$: Stream.of('Please enter a valid mobile!'),
+    label$: Stream.of('Mobile'),
+    pattern$: Stream.of('^[987][0-9]{9}$'),
+    required$: Stream.of(true),
+    maxLength$: Stream.of(10)
+  }).dom;
+  const vdom$ =
+    Stream.combine(nameDom$, mobileDom$)
+      .map(([nameDom, mobileDom]) => 
+        section('.talk-registration', [
+          div('.title', [
+            h4(['Present your own idea'])
+          ]),
+          div('.content', [
+            form([
+              nameDom,
+              mobileDom
+            ])
+          ]),
+          div('.footer', [
+            a('.mdl-js-button.mdl-js-ripple-effect', ['Submit'])
+          ])
         ])
-      ]),
-      div('.footer', [
-        a('.mdl-js-button.mdl-js-ripple-effect', ['Submit'])
-      ])
-    ])
-  );
+      );
   return {
     dom: vdom$
   };
