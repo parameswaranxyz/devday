e0f7e2bac4b2c68770913f9b122b8876163ba4ea
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-19 16:19:09 +0530

    Add multiline option to TextField

--- src/components/TextField/index.ts
+++ src/components/TextField/index.ts
@@ -1,5 +1,5 @@
 import { Stream } from 'xstream';
-import { VNode, div, input, label, span } from '@cycle/dom';
+import { VNode, div, input, textarea, label, span } from '@cycle/dom';
 import './styles.scss';
 
 interface Sources {
@@ -8,25 +8,30 @@ interface Sources {
   pattern$?: Stream<string>;
   label$: Stream<string>;
   error$?: Stream<string>;
-  required$: Stream<boolean>;
+  required$?: Stream<boolean>;
   maxLength$?: Stream<number>;
+  rows$?: Stream<number>;
 }
 
 interface Sinks {
   dom: Stream<VNode>;
 }
 
-export const TextField = ({ id$, type$, pattern$, label$, error$, required$, maxLength$ }: Sources): Sinks => {
+export const TextField = ({ id$, type$, pattern$, label$, error$, required$, maxLength$, rows$ }: Sources): Sinks => {
   type$ = type$ || Stream.of('text');
   pattern$ = pattern$ || Stream.of(undefined);
   error$ = error$ || Stream.of(undefined);
   maxLength$ = maxLength$ || Stream.of(undefined);
+  rows$ = rows$ || Stream.of(1);
+  required$ = required$ || Stream.of(false);
   const vdom$ =
     Stream
-      .combine(id$, type$, pattern$, label$, error$, required$, maxLength$)
-      .map(([id, type, pattern, labelString, error, required, maxLength ]) =>
+      .combine(id$, type$, pattern$, label$, error$, required$, maxLength$, rows$)
+      .map(([id, type, pattern, labelString, error, required, maxLength, rows ]) =>
         div('.text-field.mdl-js-textfield', [
-          input('.mdl-textfield__input', { props: { type, pattern, id, required: required ? 'required' : '', maxLength } }),
+          rows == 1
+            ? input('.mdl-textfield__input', { props: { type, pattern, id, required: required ? 'required' : '', maxLength } })
+            : textarea('.mdl-textfield__input', { props: { id, required: required ? 'required' : '', maxLength, rows } }),
           label('.mdl-textfield__label', { attrs: { for: id } }, [labelString]),
           !!error ? span('.mdl-textfield__error', [error]) : null
         ])
