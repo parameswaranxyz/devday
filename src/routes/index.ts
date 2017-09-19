607ba875bfd03c9334eefe8043fe7f39e9192df7
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-19 21:19:44 +0530

    Refactor route resolution

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,1 @@
-import { RouteResolution, RouteDefinitions } from './definitions';
-import { Sources, Sinks } from '../definitions';
-import { routes } from './routes';
-import switchPath from 'switch-path';
-
-function resolveImplementation(routes: RouteDefinitions, route: string): RouteResolution {
-  const { path, value } = switchPath((route || '#/').replace('#',''), routes);
-  const resolution = value as RouteResolution;
-  return {
-    path: path,
-    component: resolution.component,
-    sources: resolution.sources
-  };
-}
-
-export const resolve = (route: string): RouteResolution => {
-  return resolveImplementation(routes, route);
-};
+export { resolve } from './resolve';
