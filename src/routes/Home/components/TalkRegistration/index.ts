0934c9e975c2c13df92b794c14f7a7aecebf5339
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-19 16:19:18 +0530

    Add a simple form for presentations

--- src/routes/Home/components/TalkRegistration/index.ts
+++ src/routes/Home/components/TalkRegistration/index.ts
@@ -8,31 +8,49 @@ interface Sinks {
 }
 
 export const TalkRegistration = (): Sinks => {
+  const titleDom$ = TextField({
+    id$: Stream.of('talk-title'),
+    error$: Stream.of('Please enter a title!'),
+    label$: Stream.of('Title')
+  }).dom;
+  const abstractDom$ = TextField({
+    id$: Stream.of('talk-abstract'),
+    error$: Stream.of('Please enter an abstract!'),
+    label$: Stream.of('Abstract'),
+    rows$: Stream.of(5)
+  }).dom;
   const nameDom$ = TextField({
     id$: Stream.of('talk-name'),
     error$: Stream.of('Please enter a valid name!'),
     label$: Stream.of('Name'),
-    pattern$: Stream.of('^[a-zA-Z][a-zA-Z ]{4,}'),
-    required$: Stream.of(true)
+    pattern$: Stream.of('^[a-zA-Z][a-zA-Z ]{4,}')
+  }).dom;
+  const emailDom$ = TextField({
+    id$: Stream.of('talk-email'),
+    error$: Stream.of('Please enter a valid email!'),
+    label$: Stream.of('Email'),
+    type$: Stream.of('email')
   }).dom;
   const mobileDom$ = TextField({
     id$: Stream.of('talk-mobile'),
     error$: Stream.of('Please enter a valid mobile!'),
     label$: Stream.of('Mobile'),
     pattern$: Stream.of('^[987][0-9]{9}$'),
-    required$: Stream.of(true),
     maxLength$: Stream.of(10)
   }).dom;
   const vdom$ =
-    Stream.combine(nameDom$, mobileDom$)
-      .map(([nameDom, mobileDom]) => 
+    Stream.combine(titleDom$, abstractDom$, nameDom$, emailDom$, mobileDom$)
+      .map(([titleDom, abstractDom, nameDom, emailDom, mobileDom]) => 
         section('.talk-registration', [
           div('.title', [
             h4(['Present your own idea'])
           ]),
           div('.content', [
             form([
+              titleDom,
+              abstractDom,
               nameDom,
+              emailDom,
               mobileDom
             ])
           ]),
