3e603a956a1e892ec85a98bb1ba4ea5eddcbbe7f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-21 21:05:32 +0530

    Update tests to reflect changes to routes

--- src/routes/resolve.spec.ts
+++ src/routes/resolve.spec.ts
@@ -3,17 +3,23 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('routes.resolve', () => {
-  it('should resolve / to Home', () => {
+  it('should resolve / to Home', (done) => {
     const resolution = resolve('/');
     expect(resolution).to.not.be.null;
-    expect(resolution.component).to.not.be.null;
-    expect(resolution.component.name).to.equal('Home');
+    resolution.getComponent().then(component => {
+      expect(component).to.not.be.null;
+      expect(component.name).to.equal('Home');
+      done();
+    });
   });
 
-  it('should resolve /events/ to EventDetail', () => {
+  it('should resolve /events/ to EventDetail', (done) => {
     const resolution = resolve('/events/some-event-url-here');
     expect(resolution).to.not.be.null;
-    expect(resolution.component).to.not.be.null;
-    expect(resolution.component.name).to.equal('EventDetail');
+    resolution.getComponent().then(component => {
+      expect(component).to.not.be.null;
+      expect(component.name).to.equal('EventDetail');
+      done();
+    });
   });
 });
