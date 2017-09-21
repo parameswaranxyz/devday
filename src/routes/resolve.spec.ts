fd54f6e2ba1b39ee7740b27f2a4968fda5a5324d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-21 21:13:35 +0530

    Return promises

--- src/routes/resolve.spec.ts
+++ src/routes/resolve.spec.ts
@@ -3,23 +3,21 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('routes.resolve', () => {
-  it('should resolve / to Home', (done) => {
+  it('should resolve / to Home', () => {
     const resolution = resolve('/');
     expect(resolution).to.not.be.null;
-    resolution.getComponent().then(component => {
+    return resolution.getComponent().then(component => {
       expect(component).to.not.be.null;
       expect(component.name).to.equal('Home');
-      done();
     });
   });
 
-  it('should resolve /events/ to EventDetail', (done) => {
+  it('should resolve /events/ to EventDetail', () => {
     const resolution = resolve('/events/some-event-url-here');
     expect(resolution).to.not.be.null;
-    resolution.getComponent().then(component => {
+    return resolution.getComponent().then(component => {
       expect(component).to.not.be.null;
       expect(component.name).to.equal('EventDetail');
-      done();
     });
   });
 });
