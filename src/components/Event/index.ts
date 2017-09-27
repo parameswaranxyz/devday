361abfafbe3728997cb8f4d14215ef871dce6626
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-09-27 12:20:21 +0530

    Use EventRegistration component

--- src/components/Event/index.ts
+++ src/components/Event/index.ts
@@ -1,10 +1,11 @@
 import { Stream } from 'xstream';
 import { div, article, h2, h3, button, p, VNode, DOMSource } from '@cycle/dom';
 import isolate from '@cycle/isolate';
-import { DevdayEvent} from '../../definitions';
+import { DevdayEvent } from '../../definitions';
 import sampleCombine from 'xstream/extra/sampleCombine';
 import * as moment from 'moment';
 import * as marked from 'marked';
+import { EventRegistration } from '../EventRegistration';
 import './styles.scss';
 
 interface Sources {
@@ -18,38 +19,41 @@ interface Sinks {
 }
 
 const fadeInOutStyle = {
-  opacity: '0', delayed: {opacity: '1'}, remove: {opacity: '0'}
+  opacity: '0', delayed: { opacity: '1' }, remove: { opacity: '0' }
 };
 
 const EventComponent = ({ dom, event$ }: Sources): Sinks => {
+  const xs = Stream;
   const viewDetailsClick$ = dom.select('.action').events('click', { preventDefault: true });
   const navigateTo$ =
     viewDetailsClick$.compose(sampleCombine(event$))
-      .map(([_, { meetup_event_id, meetup_urlname }]) =>
-        `https://meetup.com/${meetup_urlname}/events/${meetup_event_id}`
-      );
-  const vtree$ = event$.map(({ title, event_time: { start_time }, abstract, venue: { city }, image_url, url, meetup_event_id, meetup_urlname }) =>
-    article('.event', { style: fadeInOutStyle, key: url }, [
-      div('.media', { style: { delayed: { 'background-image': `url("${image_url}")` } } }, [
-        div('.overlay', { style: fadeInOutStyle }, [city])
-      ]),
-      div('.content', [
-        div('.header', [
-          h2('.title', { attrs: { title: title }, style: fadeInOutStyle }, [ title ]),
-          h3('.subtitle', { style: fadeInOutStyle }, [moment(start_time).format('llll')])
-        ]),
-        div('.separator'),
-        div('.description', { props: { innerHTML: marked(abstract) }, style: fadeInOutStyle }),
-        div('.footer', [
-          !!meetup_urlname && !!meetup_event_id ? button('.action', { style: fadeInOutStyle }, ['View Details']) : null
+      .map(([_, { meetup_event_id, meetup_urlname }]) => `https://meetup.com/${meetup_urlname}/events/${meetup_event_id}`);
+  const eventRegistration = EventRegistration({ dom });
+  const vtree$ =
+    xs.combine(event$, eventRegistration.dom)
+      .map(([{ title, event_time: { start_time }, abstract, venue: { city }, image_url, url, meetup_event_id, meetup_urlname }, eventRegistration]) =>
+        article('.event', { style: fadeInOutStyle, key: url }, [
+          div('.media', { style: { delayed: { 'background-image': `url("${image_url}")` } } }, [
+            div('.registrations', [eventRegistration]),
+            div('.overlay', { style: fadeInOutStyle }, [city])
+          ]),
+          div('.content', [
+            div('.header', [
+              h2('.title', { attrs: { title: title }, style: fadeInOutStyle }, [title]),
+              h3('.subtitle', { style: fadeInOutStyle }, [moment(start_time).format('llll')])
+            ]),
+            div('.separator'),
+            div('.description', { props: { innerHTML: marked(abstract) }, style: fadeInOutStyle }),
+            div('.footer', [
+              !!meetup_urlname && !!meetup_event_id ? button('.action', { style: fadeInOutStyle }, ['View Details']) : null
+            ])
+          ])
         ])
-      ])
-    ])
-  );
+      );
   navigateTo$.addListener({ next: location => window.open(location, '_blank') });
   return {
     dom: vtree$,
-    history: Stream.empty()
+    history: xs.empty()
   };
 }
 
